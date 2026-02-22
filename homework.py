02d1ce200f954373bb4586fd70a9ee8a78637f45
lucillemaicalderbank <lcalderbank26@bentleyschool.org>
2026-02-21 20:40:45 -0800

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,31 @@
+#Please modify the below functions so they fulfill the described process.
+#You must use a function from analytics.py in each question to receive credit.
+#There is no provided test file. You must make and submit one yourself. (check older test files for reference)
+
+
+# Modify the below function such that it takes in a list of prices and returns that list with 15% added value
+def process_expenses(rawPrices):
+    pass
+
+
+# Modify the below function such that it asks the user for n scores and then returns the highest score and the average score of the list.
+def analyze_scores(n):
+    pass
+
+# Modify the below function such that it takes in a list of strings and returns that list with all spaces removed
+#and all letters lower case.
+def sanitize_usernames():
+   pass
+
+
+# Modify the list such that it takes in a list as an argument and returns a version of the list with all values over 100.
+def identify_outliers():
+    pass
+
+
+# Modify the below function such that it takes in a list of items and asks the user for an item to search for.
+#Sanitize the list to only be lower case worsd with no extra spaces
+#Then return the location of the word using binary search if the list is in order and linear search if it is not.
+#example items = ["  Apple", "Banana ", "  CHERRY  ", " date "]
+def search_and_report():
+    pass
