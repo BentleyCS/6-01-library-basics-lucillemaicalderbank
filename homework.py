be084ab6b7b13ad93b81e994ba34663e7be0afa5
lucillemaicalderbank <lcalderbank26@bentleyschool.org>
2026-02-21 20:44:26 -0800

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,31 +1,31 @@
-#Please modify the below functions so they fulfill the described process.
-#You must use a function from analytics.py in each question to receive credit.
-#There is no provided test file. You must make and submit one yourself. (check older test files for reference)
+def search_and_report(items):
+    # Use analytics to clean the list
+    clean_items = analytics.clean_text(items)
 
+    target = input("Enter item to search for: ").strip().lower()
 
-# Modify the below function such that it takes in a list of prices and returns that list with 15% added value
-def process_expenses(rawPrices):
-    pass
+    # Check if the list is already sorted manually
+    is_ordered = True
+    for i in range(len(clean_items) - 1):
+        if clean_items[i] > clean_items[i + 1]:
+            is_ordered = False
+            break
 
+    if is_ordered:
+        # Binary Search
+        low, high = 0, len(clean_items) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if clean_items[mid] == target:
+                return mid
+            elif clean_items[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+    else:
+        # Linear Search
+        for i in range(len(clean_items)):
+            if clean_items[i] == target:
+                return i
 
-# Modify the below function such that it asks the user for n scores and then returns the highest score and the average score of the list.
-def analyze_scores(n):
-    pass
-
-# Modify the below function such that it takes in a list of strings and returns that list with all spaces removed
-#and all letters lower case.
-def sanitize_usernames():
-   pass
-
-
-# Modify the list such that it takes in a list as an argument and returns a version of the list with all values over 100.
-def identify_outliers():
-    pass
-
-
-# Modify the below function such that it takes in a list of items and asks the user for an item to search for.
-#Sanitize the list to only be lower case worsd with no extra spaces
-#Then return the location of the word using binary search if the list is in order and linear search if it is not.
-#example items = ["  Apple", "Banana ", "  CHERRY  ", " date "]
-def search_and_report():
-    pass
+    return -1
